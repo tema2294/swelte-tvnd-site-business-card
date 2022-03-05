73f22128caa937766cb2061657c7fcac74b28d5d
tema2294 <tema.marinin@mail.ru>
2022-03-05 23:51:35 +0300

    add new section (category)

---
+++ src/Category-cards.svelte
@@ -0,0 +1,100 @@
+<script>
+    import {Link} from "svelte-routing";
+</script>
+<div class="category-list">
+    <div class="card">
+        <img height="140" class="card-img" src="https://автостанция63.рф/img/catalog34.png"/>
+
+        <div class="content-card">
+            <span class="header-card">Тнвд</span>
+            <!--            <div class="header-text">Ящерицы — это широко распространенная группа чешуйчатых рептилий, насчитывающая-->
+            <!--                более 6000 видов на всех континентах, кроме Антарктиды.-->
+            <!--            </div>-->
+        </div>
+        <div class="actions-container">
+
+        </div>
+    </div>
+    <div class="card">
+        <img height="140" class="card-img" src="https://diesel-exp.ru/upload/iblock/96c/96c252f19a43a8c4309559b87e9142e5.jpg"/>
+        <div class="content-card">
+            <span class="header-card">Плунжерные пары</span>
+        </div>
+        <div class="actions-container">
+        </div>
+    </div>
+    <div class="card">
+        <img height="140" class="card-img" src="https://dizelstore.ru/image/cache/catalog/tovar/%20%D0%90%D0%97%D0%9F%D0%98-500x500.jpg"/>
+        <div class="content-card">
+            <span class="header-card">Распылители</span>
+        </div>
+        <div class="actions-container">
+        </div>
+    </div>
+    <div class="card">
+        <img height="140" class="card-img" src="https://www.enisey-servis.ru/upload/resize_cache/iblock/04e/244_172_1/04e023ef096d5b495bc204e2b5dc01d0.jpg"/>
+        <div class="content-card">
+            <span class="header-card">ТННД</span>
+        </div>
+        <div class="actions-container">
+        </div>
+    </div>
+    <div class="card">
+        <img height="140" class="card-img" src="https://cdn-icons-png.flaticon.com/512/64/64722.png"/>
+        <div class="content-card">
+            <span class="header-card">Другое</span>
+        </div>
+        <div class="actions-container">
+        </div>
+    </div>
+</div>
+<style>
+
+
+    .card {
+        transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+        border-radius: 4px;
+        box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+        width: 300px;
+        background-color: #fff !important;
+        flex-shrink: 0;
+        overflow: hidden;
+    }
+
+    .card-img {
+        width: 100%;
+        object-fit: contain;
+    }
+
+    .header-card {
+        font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+        font-weight: 400;
+        font-size: 1.5rem;
+        line-height: 1.334;
+        letter-spacing: 0em;
+        margin-bottom: 0.35em;
+    }
+
+    .header-text {
+        margin: 0;
+        font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+        font-weight: 400;
+        font-size: 0.875rem;
+        line-height: 1.43;
+        letter-spacing: 0.01071em;
+        color: black !important;
+        white-space: normal;
+
+    }
+
+    .content-card {
+        padding: 16px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        width: 100%;
+    }
+
+
+</style>
---
+++ src/MainPage.svelte
@@ -0,0 +1,116 @@
+<script>
+    import Category from "./category-cards.svelte";
+
+    function customScroll(type) {
+        const sections = document.querySelector(type);
+        console.log(sections)
+        return sections.scrollIntoView({behavior: 'smooth', block: 'center'})
+    }
+
+    function downloadFile() {
+        const link = document.createElement('a');
+        link.setAttribute('href', '/Microsoft Excel Worksheet.xlsx');
+        link.setAttribute('download', 'price.xlsx');
+        link.click();
+        return false;
+    }
+
+    let isOpen = false;
+
+</script>
+
+<main>
+    <header>
+        <nav class="nav-container">
+            <a>ТНВД 76</a>
+            <div on:click={()=> isOpen = !isOpen} class="{!isOpen ? 'burger_menu' : 'burger_menu burger_menu-open'}">
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="{!isOpen ? 'nav-list' : 'nav-list nav-list-open'}">
+                <li on:click={()=>customScroll('.info-section')}>О нас</li>
+                <li on:click={()=>customScroll('.advantages-section')}>Преимущества</li>
+                <li on:click={()=>customScroll('.address-section')}>Контакты</li>
+                <li on:click={()=>customScroll('.map-section')}>Как добраться</li>
+            </ul>
+            <a class="nav-info" href="tel:+79066359997">
+                <div class="nav-info_work-time">Пн-Вс 10:00 - 21:00</div>
+                <div class="nav-info_work-number">+ 7 906 635 9997</div>
+            </a>
+        </nav>
+
+    </header>
+    <section class="main-section">
+        <div class="info-container">
+            <div class="info-container_title">Дизель центр в Ярославле,<br/> качество и гарантия!</div>
+            <div class="info-container_text">Ремонт и продажа Тнвд и комплектующих</div>
+            <div on:click={downloadFile} class="info-container_btn">Скачать прайс</div>
+        </div>
+    </section>
+    <section class="category">
+        <div class="category-header">Категории</div>
+            <Category></Category>
+
+    </section>
+    <section class="info-section">
+        <div class="info-section_header">О нас</div>
+        <div class="info-section_main">
+            <p>
+                Интернет магазин новых и б/у запчастей.У нас вы можете купить запчасти по всей России. Команда
+                профессионалов специализируется на подборе и доставке запчастей для ремонта и обслуживания ТНВД.
+            </p>
+            <p>Мы делаем полный спектр ремонтных работ , от диагностики до замены любой
+                запчасти. Вы всегда получаете качественную работу от наших профессионалов и полную гарантию на все виды
+                работ.
+            </p>
+            <!--            <p>Мы ремонтируем марки автомобилей: <b>Toyota, Honda, Subaru, Nissan, Suzuki, Mazda, Mitsubishi</b></p>-->
+        </div>
+    </section>
+    <section class="advantages-section">
+        <div class="advantages-section_title">
+            Наши преимущества
+        </div>
+
+        <div class="advantages-section_list">
+            <div class="card">
+                <img class="card_img" src="img/vote-icon-19.png" alt=""/>
+                <div class="card_title">Наличие</div>
+                <div class="card_text">Большой перечень запчастей в наличие</div>
+            </div>
+            <div class="card">
+                <img class="card_img" src="img/342-3426164_5-year-f.png" alt=""/>
+                <div class="card_title">Гарантия</div>
+                <div class="card_text">Полное гарантийное обслуживание на всю продукцию</div>
+            </div>
+            <div class="card">
+                <img class="card_img" src="img/png-clipart-computer.png" alt=""/>
+                <div class="card_title">Обязательства</div>
+                <div class="card_text">выполнения любого объема работы в срок</div>
+            </div>
+        </div>
+    </section>
+    <section class="address-section">
+        <div class="address-card">
+            <div class="address-section_title">
+                <span>Наш телефон:</span> <span>+7 906 635 9997</span>
+            </div>
+            <hr/>
+            <div class="address-section_text">
+                Москва СЗАО ул. Максимова, д. 28. Метро Горки
+                Мы находимся в большом ангарном помещении на северо-западе города. Подъезд со стороны Зеленой улицы.
+                <br>
+                <br>
+                Пн-Вс: 10:00–21:00
+            </div>
+        </div>
+
+    </section>
+    <section class="map-section">
+        <script type="text/javascript" charset="utf-8" async
+                src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A76f664072c8f87f0f18db2865e6b49de92288d0f01ace595715ad5b457ea3076&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true">
+        </script>
+    </section>
+</main>
+
+<style>
+</style>
